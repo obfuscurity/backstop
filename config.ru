b5c29ba0ea24fa7a43b9330727bb41574195e881
obfuscurity <jason@dixongroup.net>
2012-09-25 13:48:54 -0400

    use single quotes where possible

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 $:.unshift File.dirname(__FILE__) + '/lib'
-require "backstop/web"
+require 'backstop/web'
 
 run Backstop::Application
--- lib/backstop/collectd/plugins/conntrack.rb
+++ lib/backstop/collectd/plugins/conntrack.rb
@@ -2,7 +2,7 @@ class CollectdData
   # conntrack stats
   def parse_plugin_conntrack
     [{
-      metric: "conntrack.connections",
+      metric: 'conntrack.connections',
       value: data['values'][0]
     }]
   end
