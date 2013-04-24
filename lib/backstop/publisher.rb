72ffdbb705f3351cb29995396294cfe0330dd626
obfuscurity <jason@dixongroup.net>
2013-04-23 21:39:25 -0400

    try raw sockets so we can set our timeout

--- lib/backstop/publisher.rb
+++ lib/backstop/publisher.rb
@@ -4,8 +4,23 @@ module Backstop
 
     def initialize(urls, opts={})
       @connections = []
-      @connections = urls.map { |u| URI.parse(u) }.map { |u| TCPSocket.new(u.host, u.port) }
+      @connections = urls.map { |u| URI.parse(u) }.map { |u| connect_to(u.host, u.port, 1) }
       @api_key = opts[:api_key]
+    end
+
+    def connect_to(host, port, timeout)
+      addr = Socket.getaddrinfo(host, nil)
+      sock = Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0)
+
+      if timeout
+        secs = Integer(timeout)
+        usecs = Integer((timeout - secs) * 1_000_000)
+        optval = [secs, usecs].pack("l_2")
+        sock.setsockopt Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, optval
+        sock.setsockopt Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, optval
+      end
+      sock.connect(Socket.pack_sockaddr_in(port, addr[0][3]))
+      sock
     end
 
     def metric_name(name)
